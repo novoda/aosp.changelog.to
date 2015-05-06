9d9b41f3c369a8f425e6f580bab6b4b8e48b85de
Daniele Bonaldo <dany.bony@gmail.com>
2015-05-06 18:26:58 +0100

    Retrieve the correct tag, previous to the target one
    Depending on the android version, not on the tag date

--- generate_last_changelog.sh
+++ generate_last_changelog.sh
@@ -12,12 +12,24 @@ cp get_project_gitlog.sh $AOSP_DIRECTORY/
 cp gitlog_to_html $AOSP_DIRECTORY/
 cp -r html_templates $AOSP_DIRECTORY/
 
-# Retrieve the target tag and the one right before
+# Retrieve the target tag
 cd $WORK_DIRECTORY
 LAST_TAGS=$(git for-each-ref refs/tags --sort=-taggerdate --format='%(refname)' | grep -o 'android\-[0-9][\.0-9]*_.*')
 TAGS_ARRAY=(`echo $LAST_TAGS | tr "," "\n"`)
 TARGET_TAG=${TAGS_ARRAY[0]}
-PREVIOUS_TAG=${TAGS_ARRAY[1]}
+
+# Retrieve the tag right before the target one
+LAST_TAGS=$(git for-each-ref refs/tags --sort=refname --format='%(refname)'  | grep -o 'android\-[0-9][\.0-9]*_.*')
+TAGS_ARRAY=(`echo $LAST_TAGS | tr "," "\n"`)
+
+for (( t=1; t<${#TAGS_ARRAY[@]}; t++ ))
+do
+   if [ ${TAGS_ARRAY[$t]} = $TARGET_TAG ]
+   then
+     PREVIOUS_TAG=${TAGS_ARRAY[$t-1]}
+     break
+   fi        
+done
 
 echo "Generating changelog from $PREVIOUS_TAG to $TARGET_TAG"
 
